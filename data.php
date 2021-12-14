50548c6c978f294382d429ced4b76b56f0a19b44
Ibtisam Tahir <88763995+TahirIbtisam@users.noreply.github.com>
2021-12-14 20:57:55 +0500

    Add files via upload

---
+++ data.php
@@ -0,0 +1,79 @@
+<?php 
+echo json_encode("[
+    [
+      "events"=> [
+        [
+          "endTimeStamp"=> 0,
+          "name"=> "Event0",
+          "nextUrl"=> "",
+          "startTimeStamp"=> 0,
+          "url"=> ""
+        ],
+        [
+            "endTimeStamp"=> 0,
+            "name"=> "Event0",
+            "nextUrl"=> "",
+            "startTimeStamp"=> 0,
+            "url"=> ""
+        ],
+        [
+            "endTimeStamp"=> 0,
+            "name"=> "Event0",
+            "nextUrl"=> "",
+            "startTimeStamp"=> 0,
+            "url"=> ""
+        ],
+        [
+            "endTimeStamp"=> 0,
+            "name"=> "Event0",
+            "nextUrl"=> "",
+            "startTimeStamp"=> 0,
+            "url"=> ""
+        ],
+        [
+            "endTimeStamp"=> 0,
+            "name"=> "Event0",
+            "nextUrl"=> "",
+            "startTimeStamp"=> 0,
+            "url"=> ""
+        ],
+        [
+            "endTimeStamp"=> 0,
+            "name"=> "Event0",
+            "nextUrl"=> "",
+            "startTimeStamp"=> 0,
+            "url"=> ""
+        ],
+        [
+            "endTimeStamp"=> 0,
+            "name"=> "Event0",
+            "nextUrl"=> "",
+            "startTimeStamp"=> 0,
+            "url"=> ""
+        ],
+        [
+            "endTimeStamp"=> 0,
+            "name"=> "Event0",
+            "nextUrl"=> "",
+            "startTimeStamp"=> 0,
+            "url"=> ""
+        ],
+        [
+            "endTimeStamp"=> 0,
+            "name"=> "Event0",
+            "nextUrl"=> "",
+            "startTimeStamp"=> 0,
+            "url"=> ""
+        ],
+        [
+            "endTimeStamp"=> 0,
+            "name"=> "Event0",
+            "nextUrl"=> "",
+            "startTimeStamp"=> 0,
+            "url"=> ""
+        ]
+      ],
+      "title": "Cricket0"
+    ]
+  ]");
+?>
